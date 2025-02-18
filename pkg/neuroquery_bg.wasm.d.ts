4b770f330a3e9cea90a28c203a55c520f8fff1fc
Ryan Hammonds <ryanphammonds@gmail.com>
2025-02-17 16:32:24 -0800

    add original pkg

---
+++ pkg/neuroquery_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run_query(a: number, b: number): Array;
+export function read_npz_file(a: number, b: number): Array;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __externref_table_dealloc(a: number): void;
+export function __wbindgen_start(): void;
